0f6b11ffca9e7f183861431b8d64bdef758a67de
itewqq <itewqq@email>
2026-04-27 16:36:55 +0800

    chore: drop dead source modules

--- src/debug_util.rs
+++
@@ -1,9 +0,0 @@
-// 通用调试输出宏，设置环境变量 DEBUG=1 时生效
-#[macro_export]
-macro_rules! debug_log {
-    ($($arg:tt)*) => {
-        if std::env::var("DEBUG").map(|v| v == "1").unwrap_or(false) {
-            eprintln!($($arg)*);
-        }
-    };
-}
